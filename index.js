7e00a729696140e19041d56eb6f9ba28b93ffa32
mytien <thaitiendrm@gmail.com>
2022-06-29 23:04:46 +0700

    update port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.set("view engine", "ejs");
@@ -96,6 +98,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
