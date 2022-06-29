ab730bb1e008de7256cbe3b9508a342e09552205
mytien <thaitiendrm@gmail.com>
2022-06-29 22:57:45 +0700

    update v2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.set("view engine", "ejs");
@@ -11,27 +12,87 @@ app.set("views", "./public");
 
 // Router
 app.get("/", (req, res) => {
-    res.render("login",{userNames});
+  res.render("index");
 });
-app.get("/index", (req, res) => {
-    res.render("index");
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
-let messages = [];
-let userNames = [];
 
 // Socket
+const users = [];
+const messages = [];
+
 io.on("connection", (socket) => {
-  // userNames[socket.id] = name;
-  socket.emit("messages", messages);
-  socket.emit("userNames", userNames);
-  socket.on("message", (msg) => {
-    messages.push(msg);
-    io.emit("message", msg);
+  socket.on("disconnect", () => {
+    const indexUser = users.findIndex((user) => user.id === socket.id);
+    if (indexUser !== -1) {
+      users[indexUser].online = false;
+      io.emit("users", users);
+    }
   });
-  socket.on("userName", (msg) => {
-    userNames.push(msg);
-    console.log(userNames);
-    io.emit("userName", userNames);
+
+  socket.on("re-login", (data) => {
+    const { username } = data;
+    const indexUser = users.findIndex((user) => user.username === username);
+    if (indexUser !== -1) {
+      users[indexUser].id = socket.id;
+      users[indexUser].online = true;
+      io.emit("users", users);
+      socket.emit("messages", messages);
+    } else {
+      socket.emit("logout");
+    }
+  });
+
+  socket.on("login", (data) => {
+    const { username, password } = data;
+    const indexUser = users.findIndex((user) => user.username === username);
+    if (users?.[indexUser]) {
+      if (users?.[indexUser].password === password) {
+        users[indexUser].id = socket.id;
+        users[indexUser].online = true;
+        socket.emit("login", { username, success: true });
+        io.emit("users", users);
+        socket.emit("messages", messages);
+      } else {
+        socket.emit("login", { success: false, message: "Wrong password." });
+      }
+    } else {
+      socket.emit("login", { success: false, message: "User not found." });
+    }
+  });
+
+  socket.on("register", (data) => {
+    const { username, password } = data;
+    const user = users.find((user) => user.username === username);
+    if (user) {
+      socket.emit("register", {
+        success: false,
+        message: "Username already exists.",
+      });
+    } else {
+      users.push({ id: socket.id, username, password, online: true });
+      socket.emit("register", {
+        username,
+        success: true,
+        message: "Register success.",
+      });
+      io.emit("users", users);
+      socket.emit("messages", messages);
+    }
+  });
+
+  socket.on("message", (data) => {
+    const { username, message } = data;
+    const lastMessage = messages[messages.length - 1];
+    messages.push({ username, message });
+
+    io.emit("message", {
+      username,
+      message,
+      next: lastMessage?.username === username,
+    });
   });
 });
 
--- public/script.js
+++ public/script.js
@@ -1,63 +1,197 @@
-const socket = io();
-const input = document.querySelector("#chat_input");
-const formChat = document.querySelector(".chat-box-footer");
-const chatBlock = document.querySelector("#chat_messages");
-const formLogin = document.querySelector(".form-login");
-// const userName = document.querySelector('input[name="username"]');
-// const password = document.querySelector('input[name="password"]');
-const otherProfile = document.querySelector(".other-profile");
-const myProfile = document.querySelector(".my-profile");
-socket.on("userNames", (msg) => {
-  for (let index = 0; index < msg.length; index++) {
-    const span = document.createElement("span");
+// const socket = io();
+// const input = document.querySelector("#chat_input");
+// const formChat = document.querySelector(".chat-box-footer");
+// const chatBlock = document.querySelector("#chat_messages");
+// const formLogin = document.querySelector(".form-login");
+// // const userName = document.querySelector('input[name="username"]');
+// // const password = document.querySelector('input[name="password"]');
+// const otherProfile = document.querySelector(".other-profile");
+// const myProfile = document.querySelector(".my-profile");
+// socket.on("userNames", (msg) => {
+//   for (let index = 0; index < msg.length; index++) {
+//     const span = document.createElement("span");
 
-    if (index % 2 === 0) {
-      span.textContent = msg[index];
-      myProfile.appendChild(span);
-    }
-    else{
-      span.textContent = msg[index];
-      otherProfile.appendChild(span);
-    }
-  }
-  // msg.forEach((element) => {
-  //   const span = document.createElement("span");
-  //   span.textContent = element;
-  //   myProfile.appendChild(span);
-  // });
-});
-socket.on("messages", (msg) => {
-  msg.forEach((element) => {
-    createMessage(element);
-  });
-});
-socket.on("message", (msg) => {
-  createMessage(msg);
-});
+//     if (index % 2 === 0) {
+//       span.textContent = msg[index];
+//       myProfile.appendChild(span);
+//     }
+//     else{
+//       span.textContent = msg[index];
+//       otherProfile.appendChild(span);
+//     }
+//   }
+//   // msg.forEach((element) => {
+//   //   const span = document.createElement("span");
+//   //   span.textContent = element;
+//   //   myProfile.appendChild(span);
+//   // });
+// });
+// socket.on("messages", (msg) => {
+//   msg.forEach((element) => {
+//     createMessage(element);
+//   });
+// });
+// socket.on("message", (msg) => {
+//   createMessage(msg);
+// });
 
-// formLogin.addEventListener("submit", (e) => {
+// // formLogin.addEventListener("submit", (e) => {
+// //   e.preventDefault();
+// //   if (userName.value && password.value) {
+// //     socket.emit("userName", userName.value);
+// //     userName.value = "";
+// //     password.value = "";
+// //     window.location.href = "http://localhost:3000/index";
+// //   }
+// // });
+// formChat.addEventListener("submit", (e) => {
+//   console.log(111);
 //   e.preventDefault();
-//   if (userName.value && password.value) {
-//     socket.emit("userName", userName.value);
-//     userName.value = "";
-//     password.value = "";
-//     window.location.href = "http://localhost:3000/index";
+//   if (input.value) {
+//     console.log(input.value);
+//     socket.emit("message", input.value);
+//     input.value = "";
 //   }
 // });
-formChat.addEventListener("submit", (e) => {
-  console.log(111);
-  e.preventDefault();
-  if (input.value) {
-    console.log(input.value);
-    socket.emit("message", input.value);
-    input.value = "";
+// const createMessage = (msg) => {
+//   const item = document.createElement("div");
+//   item.classList.add("message");
+//   item.classList.add("my-message");
+//   item.textContent = msg;
+//   chatBlock.appendChild(item);
+//   window.scrollTo(0, document.body.scrollHeight);
+// };
+
+const socket = io();
+
+let user = {};
+if (window.location.pathname === "/login") {
+  if (localStorage.getItem("user")) {
+    window.location.href = "/";
+  }
+} else {
+  if (localStorage.getItem("user")) {
+    user = JSON.parse(localStorage.getItem("user"));
+    socket.emit("re-login", { username: user.username });
+  } else {
+    window.location.href = "/login";
+  }
+}
+
+const btnLogin = document.querySelector("#btnLogin");
+const btnRegister = document.querySelector("#btnRegister");
+const username = document.querySelector("#formLogin input[name=username]");
+const password = document.querySelector("#formLogin input[name=password]");
+
+const boxStatus = document.querySelector(".box-status");
+const formChat = document.querySelector("#formChat");
+const inputChat = document.querySelector("#inputChat");
+const btnChat = document.querySelector("#btnChat");
+const chatBody = document.querySelector("#chat_box_body");
+const chatMessage = document.querySelector("#chat_messages");
+
+btnLogin?.addEventListener("click", () => {
+  if (username.value && password.value) {
+    socket.emit("login", {
+      username: username.value,
+      password: password.value,
+    });
   }
 });
-const createMessage = (msg) => {
-  const item = document.createElement("div");
-  item.classList.add("message");
-  item.classList.add("my-message");
-  item.textContent = msg;
-  chatBlock.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+
+btnRegister?.addEventListener("click", () => {
+  if (username.value && password.value) {
+    socket.emit("register", {
+      username: username.value,
+      password: password.value,
+    });
+  }
+});
+
+formChat?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (inputChat.value) {
+    socket.emit("message", {
+      username: user.username,
+      message: inputChat.value,
+    });
+    inputChat.value = "";
+  }
+});
+
+socket.on("users", (users) => {
+  boxStatus.innerHTML = "";
+  users.forEach((user) => {
+    boxStatus.innerHTML += `<div class="message other-message"><span class="${
+      user?.online ? "online" : ""
+    }"></span>${user?.username}</div>`;
+  });
+});
+
+socket.on("login", (data) => {
+  const { username, success, message } = data;
+  if (success) {
+    localStorage.setItem("user", JSON.stringify({ username }));
+    window.location.href = "/";
+  } else {
+    alert(message);
+  }
+});
+
+socket.on("register", (data) => {
+  const { username, success, message } = data;
+  if (success) {
+    localStorage.setItem("user", JSON.stringify({ username }));
+    window.location.href = "/";
+  } else {
+    alert(message);
+  }
+});
+
+socket.on("logout", () => {
+  localStorage.removeItem("user");
+  window.location.href = "/login";
+});
+
+const createMessage = (msg, next = false) => {
+  const { username, message } = msg;
+  if (username === user.username) {
+    chatMessage.innerHTML += `
+    ${
+      next
+        ? ""
+        : `<div class="profile my-profile">
+      <img
+        src="https://images.unsplash.com/photo-1534135954997-e58fbd6dbbfc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=02d536c38d9cfeb4f35f17fdfaa36619"
+        width="30" height="30" />
+        <span>${username}</span>
+    </div>`
+    }
+    <div class="message my-message">${message}</div>`;
+  } else {
+    chatMessage.innerHTML += `
+     ${
+       next
+         ? ""
+         : `<div class="profile other-profile">
+        <img
+          src="https://images.unsplash.com/photo-1537396123722-b93d0acd8848?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=efc6e85c24d3cfdd15cd36cb8a2471ed"
+          width="30" height="30" />
+          <span>${username}</span>
+    </div>`
+     }
+    <div class="message other-message"></span>${message}</div>`;
+  }
+  chatBody.scrollTo(0, chatBody.scrollHeight);
 };
+
+socket.on("message", (data) => {
+  const { next } = data;
+  createMessage(data, next);
+});
+
+socket.on("messages", (data) => {
+  for (let i = 0; i < data.length; i += 1) {
+    createMessage(data[i], data[i]?.username === data[i - 1]?.username);
+  }
+});
